270ae9add06bd508b9aa16a3df8be36db64f3ba8
NicholasTanz <nicholastanzillo@gmail.com>
2024-12-01 17:47:54 -0500

    setup frontend

---
+++ src/frontend/vite-project/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+    <h1>Hello World</h1>
+  </div>
+</template>
+
+<script lang="ts">
+export default {
+  name: 'App',
+};
+</script>
+
+<style>
+#app {
+  font-family: Arial, sans-serif;
+  text-align: center;
+  margin-top: 50px;
+}
+</style>
