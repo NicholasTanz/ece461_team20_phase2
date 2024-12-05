ab4103dc0787c344d309866dc79ee8700f1505b0
NicholasTanz <nicholastanzillo@gmail.com>
2024-12-05 16:18:08 -0500

    remove unused depends

--- src/packageEvaluator.ts
+++ src/packageEvaluator.ts
@@ -1,10 +1,7 @@
 import { Router, Request, Response } from 'express';
 import path from 'path';
 import fs from 'fs';
-import multer from 'multer';
-import { v4 as uuidv4 } from 'uuid';
 import logger from './logger';
-import axios from 'axios';
 import { processUrl } from './run'; // Adjust the path based on your directory structure
 
 async function evaluatePackage(url: string) {
